3d96e5ecbbc707a050585e580b5e2ea2d6a3ab00
janinealves04 <janinealves.sil@gmail.com>
2024-04-04 18:03:20 -0300

    Segunda aula de exemplos de lista duplamente encadeada.

--- ED-AULAS/LISTAS-DUPLAMENTE-ENCADEADAS/A1ListaDupla.js
+++ ED-AULAS/LISTAS-DUPLAMENTE-ENCADEADAS/A1ListaDupla.js
@@ -6,15 +6,61 @@
 import NoDuplo from "./A1NoDuplo.js";
 
 class ListaDupla {
+    //criar uma lista vazia
     constructor(){
         this.inicio = null;
         this.final = null;
     }
+    //lista não é ninguem sem os metódos 
     inserirInicio(valor){
+        //insere os valores
         const novoNo = new NoDuplo(valor);
         if (this.inicio === null){
             this.inicio = novoNo;
             this.final = novoNo;
         }
+
     }
+            //ou
+        /**if (!this.inicio){
+            this.inicio = novoNo;
+            this.final = novoNo;
+        }else {
+            this.inicio.anterior =nocoNo;
+            novoNo.proximo =this.inicio;
+            this.inicio =novoNo;
+        }
+        **/
+
+    inserirFinal(valor){
+        const novoNo = new NoDuplo(valor);
+        if (this.inicio === null){
+            this.inicio = novoNo;
+            this.final = novoNo;
+    
+        }else {
+            novoNo.anterior = this.final;
+            this.final.proximo = novoNo;
+        }
+    }
+    removerInicio(){
+        //atende o caso de lista vazia
+        if (!this.inicio){
+            return null;
+            }else{
+                const valor = this.inicio.valor 
+                //atende o caso de apenas um elemento na lista 
+                if (this.inicio.proximo === null){
+                    this.inicio = null;
+                    this.final = null;
+                } else {
+                    const noProximo = this.inicio.proximo;
+                    noProximo.anterior = null;
+                    this.inicio.proximo = null;
+                    this.inicio = noProximo;
+                }
+                return valor;
+            }
+        }
+
 }
