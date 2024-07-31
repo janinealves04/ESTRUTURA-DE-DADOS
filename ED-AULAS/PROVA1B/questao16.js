2ef9c0706eafc163488bd3982c05f12a176833cf
janinealves04 <janinealves.sil@gmail.com>
2024-07-31 17:44:48 -0300

    Rascuno de raciocinio para a questão 16.

--- ED-AULAS/PROVA1B/questao16.js
+++ ED-AULAS/PROVA1B/questao16.js
@@ -1,1 +1,27 @@
 /* 16. Escreva uma função que encontre o elemento mais frequente em um array. */
+
+
+class Array {
+    constructor (){
+        this.array = [];
+    }
+
+    VerificarRepetidos (){
+        if (this.items.leght > 0){
+            let elementoA = this.item
+            if ( elementoA === elementoB );
+        }
+    }
+}
+
+/* Array = 3,3,3,4,5
+
+>pecorra o array
+>verifique se está vazio
+>se não estiver prossiga 
+>registre os elementos iguais 
+>prossiga 
+>se o elementoA for igual ao elemento b
+>retorne eles no console
+
+como fazer com um array variavel ? */
