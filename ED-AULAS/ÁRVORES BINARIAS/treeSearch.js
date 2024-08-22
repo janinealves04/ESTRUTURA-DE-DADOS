fbb8e237010ce955e17969d43d33360bfa7ceb7a
Janine Alves da Silva <20221148060025@ifrn.local>
2024-08-22 17:59:12 -0300

    Exercicio em sala de aula REMOVE E SEARCH.

---
+++ ED-AULAS/ÁRVORES BINARIAS/treeSearch.js
@@ -0,0 +1,44 @@
+/*class SearchTree {
+    constructor(){
+        this.root = null;
+        this.nodeEsquesda = null;
+        this.nodeDireita = null;
+    }
+//buscar 
+    
+    SearchTreeNode(node = this.root, valor ){
+        if(this.root !== null){
+            if (node === valor){
+                node = valor;
+                console.log(${valor});
+            } 
+            else {
+                if (this.nodeEsquerda === valor){
+                    console.log(${valor});
+                }
+            }
+        }
+       
+
+
+    };
+
+    
+};
+
+*/
+
+// node é diferente de nulo? Se sim, prossegue.
+// node você é o valor que eu quero? se sim printa valor encontrado. 
+//Se não for eu pulo pro próximo no esqueda ou direta 
+
+
+ class BinarySearchTree {
+     constructor(){
+
+     }
+
+     search (value){
+        return false ;
+     }
+ }
